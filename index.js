c43d266300c728c429bc92ac968eb9abbe5b3cf3
ymll <ymll@users.noreply.github.com>
2017-12-19 17:58:09 +0800

    Temporary hard code the new date
    bad practice.. but will change to cookie when new feature is launched

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("15 Oct, 2017 09:00:00").getTime();
+var countDownDate = new Date("23 Dec, 2017 02:45:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
     // Get todays date and time
